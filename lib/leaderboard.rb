00a524f3e0ec001ba008da4c3b1963363d8edb82
JPaul <jpaul@etsy.com>
2015-11-17 17:02:05 -0500

    Travis regex == no bueno

--- lib/leaderboard.rb
+++ lib/leaderboard.rb
@@ -18,11 +18,11 @@ class Leaderboard
   match /(decrement) (.*)?/i, prefix: ".", method: :modify_leaderboad
   match /(minus) (.*)?/i, prefix: ".", method: :modify_leaderboard
 
-  match /([a-zA-Z0-9\.]+)(\+\+)/, use_prefix: false, method: :modify_leaderboard
-  match /([a-zA-Z0-9\.]+)(--)/, use_prefix: false, method: :modify_leaderboard
+  match /([a-zA-Z0-9\._]+)(\+\+)/, use_prefix: false, method: :modify_leaderboard
+  match /([a-zA-Z0-9\._]+)(--)/, use_prefix: false, method: :modify_leaderboard
 
-  match /(\+\+)([a-zA-Z0-9\.]+)/, use_prefix: false, method: :modify_leaderboard
-  match /(--)([a-zA-Z0-9\.]+)/, use_prefix: false, method: :modify_leaderboard
+  match /(\+\+)([a-zA-Z0-9\._]+)/, use_prefix: false, method: :modify_leaderboard
+  match /(--)([a-zA-Z0-9\._]+)/, use_prefix: false, method: :modify_leaderboard
 
   def get_table_name(command)
     # Create hash with default table to pluses then add ones that are different
